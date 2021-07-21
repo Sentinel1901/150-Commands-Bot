2bc62cdd1f9c501e1e339d278c3460a907f69a76
Toxicity <82979070+Sentinel1901@users.noreply.github.com>
2021-07-21 15:22:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ exports.TOKEN = '';
 
 exports.PREFIX = '';
 
-exports.news_API = '';
+exports.news_API = '7dbe6eb877b74dc4b6e34fe6ec6da86e';
 
-exports.giphy_API = '';
+exports.giphy_API = 'CaJxgWS4z2CImmnPsbfLBuGTWLkmElVx';
 
 exports.AME_API = '';
